d9b5b79374bc46c4a832d53113765ea45eccd94c
Lady Godiva <watson.amber.denise@gmail.com>
2023-10-02 15:24:27 -0700

    updated js file

--- script.js
+++ script.js
@@ -1,78 +1,103 @@
 // Write password to the #password input
-function writePassword() {
-    var password = [preferredLength, lowercase, uppercase, numeric, special]
-  
-    var generateBtn = document.querySelector("#generate");
-  
-    var passwordText = document.querySelector("#password"); 
-  
-    var preferredLength = window.prompt("What is the preferred length of your password? (8-128 characters) ");
-      if(preferredLength >=8 && preferredLength <=128){
-        var lowercase = window.prompt("Do you want to include lowercase letters? True or False?")
-      }else {
-        window.prompt("Please choose a number between 8 and 128")
-      }
-      
-      
-      if(lowercase === true){
-      //program to generate random string from programiz.com
-      const characters = 'abcdefghijklmnopqrstuvwxyz';
-      function generateRandomString(length){
-      let result = ''; 
-      const lowercase = characters.length;
-      for (let i=0; i < length; i++ ){
-        result += characters.charAt(Math.floor(Math.random() * lowercase));
-      }
-      return result("#password");
-     }
-    }else {
-      var uppercase = window.prompt("Do you want to include uppercase letters? True or False?");
-     }
+var generateBtn = document.querySelector("#generate");
+
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+function generatePassword() {
+  //var password = [preferredLength, lowercase, uppercase, numeric, special]
+
+
+
+  var preferredLength = parseInt(window.prompt("What is the preferred length of your password? (8-128 characters) "));
+  console.log(preferredLength);
+  if  (Number.isNaN(preferredLength) || preferredLength >= 129 || preferredLength <= 7) {
+    alert("You must do it right!")
+    return generatePassword();    
     
-     if(uppercase === true){
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      function generateRandomString(length){
-      let result = ''; 
-      const uppercase = characters.length;
-      for (let i=0; i < length; i++ ){
-        result += characters.charAt(Math.floor(Math.random() * uppercase));
-      }
-      return result("#password");
-      }
-     }else {
-      var numeric = window.prompt("Do you want to include numeric characters? True or False?");
-     }
-     
-     if(numeric === true){
-      const characters = '0123456789';
-      function generateRandomString(length){
-      let result = ''; 
+  }
+  var lowercase = window.confirm("Do you want to include lowercase letters? True or False?")
+  var uppercase = window.confirm("Do you want to include uppercase letters? True or False?");
+  var numeric = window.confirm("Do you want to include numeric characters? True or False?");
+  var special = window.confirm("Do you want to include special characters? True or False?");
+  if(!lowercase && !uppercase && !numeric && !special){
+
+  }
+  var possibleChars = ""
+  if (lowercase === true) {
+    //program to generate random string from programiz.com
+    const lowercharacters = 'abcdefghijklmnopqrstuvwxyz';
+
+   possibleChars += lowercharacters
+  } 
+
+  if (uppercase === true) {
+    const uppercharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    possibleChars += uppercharacters
+  } 
+  
+
+  if (numeric === true) {
+    const characters = '0123456789';
+    function generateRandomString(length) {
+      let result = '';
       const numeric = characters.length;
-      for (let i=0; i < length; i++ ){
+      for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * numeric));
       }
-      return result("#password");
-     }
-    }else {
-      var special = window.prompt("Do you want to include special characters? True or False?");
-     }
-     
-     if(special === true){
-      const characters = '!@#$%^&*()';
-      function generateRandomString(length){
-      let result = ''; 
+      return result;
+    }
+  } 
+
+  if (special === true) {
+    const characters = '!@#$%^&*()@+//\'?:,{[]~-_.';
+  
+    function generateRandomString(length) {
+      let result = '';
       const numeric = characters.length;
-      for (let i=0; i < length; i++ ){
+      for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * special));
       }
-      return result("#password");
-  }
-     }else {
-      // Add event listener to generate button
-      generateBtn.addEventListener("click", writePassword);
-      passwordText.value = password;
-     }  
-  }
-      writePassword();
-    
-  
+      return result(document.querySelector("#password"));
+    }
+  } 
+  console.log(possibleChars)
+  //write a for loop here that loops once for each character the user would like to have in their password
+  var result = ""
+   for (let i = 0; i < preferredLength; i++) {
+        result += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+        console.log(result)
+      }
+      return result;
+}
+
+function writePassword() {
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password
+}
+
+generateBtn.addEventListener("click", writePassword);
