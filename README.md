3da0233aa58b25fb2eb16703ba26f03765074f62
Lady Godiva <watson.amber.denise@gmail.com>
2023-10-04 11:56:22 -0700

    updated README and js files as well as images

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # 02-Challenge-3
+
+## Description
+
+The purpose of this challenge was to deploy a webpage application with the ability to generate a password, based on criteria entered by the user.  This project features dynamic HTML and CSS that is updated using Javascript. The page also includes a responsive design interface for adaptation to mulitiple screen sizes.  
+
+The acceptance criteria were as follows: 
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+All acceptance criteria were met and the site functions as expected.
+
+I learned the necessity of separating global code from inner functions. For instance, in one iteration, I moved the "var generateBtn" inside the "generatePassword()" function which caused problems synactically becuase we would need to call on that variable later to initiate user prompts. I also learned new functions and methods including parseInt and number.isNaN for relative use cases. Furthermore, I understand how important it is to account for contingencies in code as a failsafe in order to not only anticipate user behavior but minimize bugs. For instance, in the initial iteration, I used prompts instead of confirms which could introduce opportunities for the user to mistype (e.g.,spelling, case, and other errors). Finally, I understand how vital notation is as well as formatting code so that it is easy to read. As the coding projects become more complex, it is critical to be able to reference and update code each step of the way.
+## Usage
+
+The html, css, and js files can be used to view the company website with web accessibility considerations applied. A link to the mockup is accessible from the repository file list. 
+![Password Generator Mockup](./assets/images/03-javascript-homework-demo.png)
+
+## Credits
+
+The project was completed with help from the course materials and organization elements discussed with the Bootcamp tutor, Erik Hirsch.
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Amber Watson]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 // Write password to the #password input
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-  //var password = [preferredLength, lowercase, uppercase, numeric, special]
 
-
-
+  //parseInt was used her to take the string value, entered by the user and turn it into a number.
   var preferredLength = parseInt(window.prompt("What is the preferred length of your password? (8-128 characters) "));
-  console.log(preferredLength);
-  //include user case when NaN is entered
-  if  (Number.isNaN(preferredLength) || preferredLength >= 129 || preferredLength <= 7) {
-    alert("You must do it right!")
-    return generatePassword();    
-    
+  //console.log(preferredLength);
+  //Number.isNan was used to correct for the user case where a NaN or (anything other than a number) was entered - suggested by tutor, Erik Hirsch.
+  //Initially I had the preferredLength criteria specified in positive terms. Including the negative case (as below) is more concise. 
+  if (Number.isNaN(preferredLength) || preferredLength >= 129 || preferredLength <= 7) {
+    alert("You must do it right! Please enter a numeric value between 8 and 128.")
+    return generatePassword();
+
   }
+  //the following are criteria prompts for each character type in our password.
   var lowercase = window.confirm("Do you want to include lowercase letters? True or False?")
   var uppercase = window.confirm("Do you want to include uppercase letters? True or False?");
   var numeric = window.confirm("Do you want to include numeric characters? True or False?");
   var special = window.confirm("Do you want to include special characters? True or False?");
-  if(!lowercase && !uppercase && !numeric && !special){
-
+  //It was suggested that I include a user case where no criteria were chosen.
+  if (!lowercase && !uppercase && !numeric && !special) {
+    alert("Please select at least one case criteria.")
+    return generatePassword();
   }
+  //this variable will house all of our (user-entered) criteria.
   var possibleChars = ""
   if (lowercase === true) {
-    //program to generate random string from programiz.com
     const lowercharacters = 'abcdefghijklmnopqrstuvwxyz';
+    // the following statement(s) specify that we will add the result of each prompt to the possibleChars variable created above. 
+    possibleChars += lowercharacters
+  }
 
-   possibleChars += lowercharacters
-  } 
 
   if (uppercase === true) {
     const uppercharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     possibleChars += uppercharacters
-  } 
-  
+  }
+
 
   if (numeric === true) {
     const numericcharacters = '0123456789';
@@ -44,25 +47,16 @@ function generatePassword() {
   if (special === true) {
     const specialcharacters = '!@#$%^&*()@+//\'?:,{[]~-_.';
     possibleChars += specialcharacters
-    
-    function generateRandomString(length) {
-      let result = '';
-      const numeric = characters.length;
-      for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * special));
-      }
-      return result(document.querySelector("#password"));
-    }
-  } 
-  console.log(possibleChars)
-  //write a for loop here that loops once for each character the user would like to have in their password
-  //generate random string from programiz.com/ refined with the help of tutor, Erik Hersch.
+
+  }
+  //console.log(possibleChars)
+  //this variable will contain the generated password (including results from the possibleChars variable above). The "generate random string" for-loop template was taken from programiz.com and refined by tutor, Erik Hirsch. We also specified that it should not exceed the preferredLength specified by the user at the start. 
   var result = ""
-   for (let i = 0; i < preferredLength; i++) {
-        result += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
-        console.log(result)
-      }
-      return result;
+  for (let i = 0; i < preferredLength; i++) {
+    result += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+    //console.log(result)
+  }
+  return result;
 }
 
 function writePassword() {
